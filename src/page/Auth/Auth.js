f7c336e8801292160545f492505cc856836b7333
志村啓太 <keitashimura2023@gmail.com>
2023-11-03 02:36:04 +0900

    feat: LeftComponentのUI実装

--- src/page/Auth/Auth.js
+++ src/page/Auth/Auth.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import "./Auth.scss";
 
 export default function Auth() {
   return (
@@ -14,8 +15,11 @@ export default function Auth() {
 
 function LeftComponent() {
   return (
-    <Col className="auth__left">
-      <h2>左側</h2>
+    <Col className="auth__left" xs={6}>
+      <div>
+        <h2>人と話すのが怖い</h2>
+        <h2>同じ悩みを持つ仲間と悩みを話しませんか？</h2>
+      </div>
     </Col>
   );
 }
