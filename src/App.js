07aebaba6c95fd3133764203d9ebb3cdbd43e585
志村啓太 <keitashimura2023@gmail.com>
2023-11-03 01:51:38 +0900

    feat: ログインしているユーザーとログインしていないユーザーを区別する処理の実装

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from "./logo.svg";
-import "./App.scss";
+import React, {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [user, setUser] = useState({name: "志村 啓太"});
+
+  return <div>{user ? <h1>ログインしています。</h1> : <h1>ログインしていません。</h1>}</div>;
 }
-
-export default App;
