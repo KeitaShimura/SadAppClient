cae7d6d9123eade4fb8ea39e20692b2e6bd9e86b
志村啓太 <keitashimura2023@gmail.com>
2023-11-03 02:00:19 +0900

    rafactor: Prettierの実行

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export default function App() {
-  const [user, setUser] = useState({name: "志村 啓太"});
+  const [user, setUser] = useState({ name: "志村 啓太" });
 
-  return <div>{user ? <h1>ログインしています。</h1> : <h1>ログインしていません。</h1>}</div>;
+  return (
+    <div>
+      {user ? <h1>ログインしています。</h1> : <h1>ログインしていません。</h1>}
+    </div>
+  );
 }
