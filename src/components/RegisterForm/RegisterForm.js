fa255a0ae447b85bae411466c601eabc0256c078
志村啓太 <keitashimura2023@gmail.com>
2023-11-06 02:53:22 +0900

    登録フォームデータをステートに保存する処理の実装

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,44 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import "./RegisterForm.scss";
 import { Button, Col, Form, Row } from "react-bootstrap";
 
 export default function RegisterForm(props) {
-  const { setShowModal } = props;
+    const { setShowModal } = props;
+    const [formData, setFormData] = useState(initialFromValue());
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    setShowModal(false);
-  };
-  return (
-    <div className="register-form">
-      <h2>新規登録フォーム</h2>
-      <Form onSubmit={onSubmit}>
-        <Form.Group className="form-group">
-          <Form.Control type="text" placeholder="名前" />
-        </Form.Group>
-        <Form.Group className="form-group">
-          <Form.Control type="email" placeholder="メールアドレス" />
-        </Form.Group>
-        <Form.Group className="form-group">
-          <Row>
-            <Col>
-              <Form.Control type="password" placeholder="パスワード" />
-            </Col>
-            <Col>
-              <Form.Control type="password" placeholder="パスワード確認" />
-            </Col>
-          </Row>
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          登録
-        </Button>
-      </Form>
-    </div>
-  );
+    const onSubmit = (e) => {
+        e.preventDefault();
+        setShowModal(false);
+
+        console.log(formData);
+    };
+
+    const onChange = e => {
+        setFormData({ ...formData, [e.target.name]: e.target.value});
+    }
+
+    return (
+        <div className="register-form">
+            <h2>新規登録フォーム</h2>
+            <Form onSubmit={onSubmit} onChange={onChange}>
+                <Form.Group className="form-group">
+                    <Form.Control
+                        type="text"
+                        placeholder="名前"
+                        name="name"
+                        defaultValue={formData.name}
+                    />
+                </Form.Group>
+                <Form.Group className="form-group">
+                    <Form.Control
+                        type="email"
+                        placeholder="メールアドレス"
+                        name="email"
+                        defaultValue={formData.email}
+                        />
+                </Form.Group>
+                <Form.Group className="form-group">
+                    <Row>
+                        <Col>
+                            <Form.Control
+                               type="password"
+                               placeholder="パスワード"
+                               name="password"
+                               defaultValue={formData.password}
+                               />
+                        </Col>
+                        <Col>
+                            <Form.Control
+                                type="password"
+                                placeholder="パスワード確認"
+                                name="confirmPassword"
+                                defaultValue={formData.confirmPassword}
+                                />
+                        </Col>
+                    </Row>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    登録
+                </Button>
+            </Form>
+        </div>
+    );
 }
 
 RegisterForm.propTypes = {
-  setShowModal: PropTypes.func.isRequired,
+    setShowModal: PropTypes.func.isRequired,
 };
+
+function initialFromValue() {
+    return {
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    }
+}
