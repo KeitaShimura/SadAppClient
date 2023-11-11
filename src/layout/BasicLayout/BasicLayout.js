a01e58ea2b4ace86280b21984a26acad4d863c61
志村啓太 <keitashimura2023@gmail.com>
2023-11-11 21:24:07 +0900

    feat: サイドバーのベース作成

--- src/layout/BasicLayout/BasicLayout.js
+++ src/layout/BasicLayout/BasicLayout.js
@@ -2,19 +2,27 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import "./BasicLayout.scss";
+import { Col, Container, Row } from "react-bootstrap";
 
 export default function BasicLayout(props) {
-  const { children } = props;
+  const { className, children } = props;
 
   return (
-    <div>
-      <h2>BasicLayout...</h2>
-      {children}
-    </div>
+    <Container className={`basic-layout ${className}`}>
+      <Row>
+        <Col xs={3} className="basic-layout__menu">
+          <h2>Menu...</h2>
+        </Col>
+        <Col xs={9} className="basic-layout__content">
+          {children}
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
 // propTypesでプロパティの型情報を指定
 BasicLayout.propTypes = {
   children: PropTypes.node,
+  className: PropTypes.string,
 };
