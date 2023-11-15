ee3820f75164c036b0dffc8394041369e121e436
志村啓太 <keitashimura2023@gmail.com>
2023-11-16 05:52:51 +0900

    feat: エラーページの作成

--- src/page/Error404/Error404.js
+++ src/page/Error404/Error404.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import Logo from "../../assets/png/error-404.png";
 import "./Error404.scss";
+import { Link } from "react-router-dom";
 
 export default function Error404() {
   return (
-    <div>
-      <h2>Error404</h2>
+    <div className="error404">
+      <h2>COCOLOTalk</h2>
+      <img src={Logo} alt="Error404" />
+      <Link to="/">ホームへ戻る</Link>
     </div>
   );
 }
