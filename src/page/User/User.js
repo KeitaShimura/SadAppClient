7d3f4eb44c60d93b035b20939fa23ec3992a0c05
志村啓太 <keitashimura2023@gmail.com>
2023-11-16 23:31:56 +0900

    feat: ユーザー名の取得

--- src/page/User/User.js
+++ src/page/User/User.js
@@ -1,21 +1,38 @@
-import React from "react";
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import BasicLayout from "../../layout/BasicLayout";
 import "./User.scss";
+import { getUserApi } from "../../api/user";
+import { toast } from "react-toastify";
 
 function User() {
   const location = useLocation();
   const params = useParams();
   const navigate = useNavigate();
+  const [user, setUser] = useState(null);
 
   console.log(location);
   console.log(params.id);
   console.log(navigate);
+  if (user) {
+    console.log(user.name);
+  }
+  useEffect(() => {
+    getUserApi(params.id)
+      .then((response) => {
+        console.log(response);
+        setUser(response);
+        if (!response) toast.error("このユーザーは存在しません。");
+      })
+      .catch(() => {
+        toast.error("このユーザーは存在しません。");
+      });
+  }, [params]);
 
   return (
     <BasicLayout className="user">
       <div className="user__title">
-        <h2>志村 啓太</h2>
+        <h2>{user ? user.name : "このユーザーは存在しません。"}</h2>
       </div>
       <div>バナー</div>
       <div>ユーザー情報</div>
