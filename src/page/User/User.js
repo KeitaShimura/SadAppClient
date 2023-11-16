9629dc2c0aaba6ae9642f9e4647cfc34f1482223
志村啓太 <keitashimura2023@gmail.com>
2023-11-16 22:11:38 +0900

    feat: ユーザーページの大枠の構造作成

--- src/page/User/User.js
+++ src/page/User/User.js
@@ -1,10 +1,16 @@
 import React from "react";
+import BasicLayout from "../../layout/BasicLayout";
 import "./User.scss";
 
 export default function User() {
   return (
-    <div>
-      <h2>User...</h2>
-    </div>
+    <BasicLayout className="user">
+      <div className="user__title">
+        <h2>志村 啓太</h2>
+      </div>
+      <div>バナー</div>
+      <div>ユーザー情報</div>
+      <div className="user__posts">投稿一覧</div>
+    </BasicLayout>
   );
 }
