34d6422c1dadb665a679ed2d0949b70c645cd15c
志村啓太 <keitashimura2023@gmail.com>
2023-11-17 00:52:33 +0900

    feat: Icon画像表示機能の実装

--- src/components/User/BannerAvatar/BannerAvatar.js
+++ src/components/User/BannerAvatar/BannerAvatar.js
@@ -1,30 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+import IconNotFound from "../../../assets/png/icon-no-found.png";
 import "./BannerAvatar.scss";
 
 export default function BannerAvatar(props) {
   const { user } = props;
+  const iconUrl = user?.icon ? user.icon : IconNotFound;
   const bannerUrl = user?.banner ? user.banner : null;
   console.log(bannerUrl);
   console.log(user);
 
   return (
     <div>
-      {bannerUrl && (
-        <div
-          className="banner-avatar"
-          style={{ backgroundImage: `url(${bannerUrl})` }}
-        >
-          {/* Other content if needed */}
-        </div>
-      )}
-      {!bannerUrl && <h2>バナーはありません</h2>}
+      <div
+        className="banner-icon"
+        style={{ backgroundImage: `url(${bannerUrl})` }}
+      >
+        <div className="icon" style={{ backgroundImage: `url(${iconUrl})` }} />
+      </div>
     </div>
   );
 }
 
 BannerAvatar.propTypes = {
   user: PropTypes.shape({
+    icon: PropTypes.string,
     banner: PropTypes.string,
   }),
 };
