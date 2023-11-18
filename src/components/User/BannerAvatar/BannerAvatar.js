f5a87a0563109f0541655aae0bda0ef300bcc89b
志村啓太 <keitashimura2023@gmail.com>
2023-11-18 22:06:12 +0900

    feat: プロフィール編集モーダルの作成

--- src/components/User/BannerAvatar/BannerAvatar.js
+++ src/components/User/BannerAvatar/BannerAvatar.js
@@ -1,39 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import IconNotFound from "../../../assets/png/icon-no-found.png";
 import { Button } from "react-bootstrap";
+import ConfigModal from "../../Modal/ConfigModal";
+
 import "./BannerAvatar.scss";
 
 export default function BannerAvatar(props) {
-  const { user, authUser } = props;
-  const iconUrl = user?.icon ? user.icon : IconNotFound;
-  const bannerUrl = user?.banner ? user.banner : null;
-  console.log(bannerUrl);
+    const { user, authUser } = props;
+    // Correct usage of useState
+    const [showModal, setShowModal] = useState(false);
+    const iconUrl = user?.icon ? user.icon : IconNotFound;
+    const bannerUrl = user?.banner ? user.banner : null;
 
-  return (
-    <div
-      className="banner-icon"
-      style={{ backgroundImage: `url(${bannerUrl})` }}
-    >
-      <div className="icon" style={{ backgroundImage: `url(${iconUrl})` }} />
-      {user && (
-        <div className="options">
-          {authUser.sub === user.id && <Button>プロフィール編集</Button>}
-          {authUser.sub !== user.id && <Button>フォローする</Button>}
+    return (
+        <div
+            className="banner-icon"
+            style={{ backgroundImage: `url(${bannerUrl})` }}
+        >
+            <div className="icon" style={{ backgroundImage: `url(${iconUrl})` }} />
+            {user && (
+                <div className="options">
+                    {authUser.sub !== user.id && <Button onClick={() => setShowModal(true)}>プロフィール編集</Button>}
+                    {authUser.sub !== user.id && <Button>フォローする</Button>}
+                </div>
+            )}
+
+            <ConfigModal show={showModal} setShow={setShowModal} title="プロフィール編集">
+                <p>dddd</p>
+                <p>dddd</p>
+                <p>dddd</p>
+                <p>dddd</p>
+                <p>dddd</p>
+                <p>dddd</p>
+                <p>dddd</p>
+                <p>dddd</p>
+                <p>dddd</p>
+                <p>dddd</p>
+            </ConfigModal>
         </div>
-      )}
-    </div>
-  );
+    );
 }
 
 BannerAvatar.propTypes = {
-  user: PropTypes.shape({
-    icon: PropTypes.string,
-    banner: PropTypes.string,
-    id: PropTypes.number, // Adding id as a required prop
-  }),
-  authUser: PropTypes.shape({
-    // Changing this to an object
-    sub: PropTypes.string,
-  }),
+    user: PropTypes.shape({
+        icon: PropTypes.string,
+        banner: PropTypes.string,
+        id: PropTypes.number,
+    }),
+    authUser: PropTypes.shape({
+        sub: PropTypes.string,
+    }),
 };
--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,7 +1,9 @@
 import { ReactComponent as LocationIcon } from "../assets/svg/location.svg";
 import { ReactComponent as LinkIcon } from "../assets/svg/link.svg";
 import { ReactComponent as DataBirthIcon } from "../assets/svg/date-birth.svg";
+import { ReactComponent as CloseIcon } from "../assets/svg/close.svg";
 
 export const Location = LocationIcon;
 export const Link = LinkIcon;
 export const DataBirth = DataBirthIcon;
+export const Close = CloseIcon;
