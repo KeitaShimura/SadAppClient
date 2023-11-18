41f4d88f3475dbe5c72fb101956bc19866963dc5
志村啓太 <keitashimura2023@gmail.com>
2023-11-18 22:06:51 +0900

    refactor: ESLint/Prettierの実行

--- src/components/Modal/ConfigModal/ConfigModal.js
+++ src/components/Modal/ConfigModal/ConfigModal.js
@@ -6,32 +6,31 @@ import { Close } from "../../../utils/icons";
 import "./ConfigModal.scss";
 
 export default function ConfigModal(props) {
-    const { show, setShow, title, children } = props;
+  const { show, setShow, title, children } = props;
 
-    return (
-        <Modal
-        className="config-modal"
-        show={show}
-        onHide={() => setShow(false)}
-        centered
-        size="lg"
-        >
-            <Modal.Header>
-                <Modal.Title>
-                    <Close onClick={() => setShow(false)} />
-                    <h2>{title}</h2>
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>{children}</Modal.Body>
-        </Modal>
-    )
+  return (
+    <Modal
+      className="config-modal"
+      show={show}
+      onHide={() => setShow(false)}
+      centered
+      size="lg"
+    >
+      <Modal.Header>
+        <Modal.Title>
+          <Close onClick={() => setShow(false)} />
+          <h2>{title}</h2>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{children}</Modal.Body>
+    </Modal>
+  );
 }
 
 // propTypesでプロパティの型情報を指定
 ConfigModal.propTypes = {
-    show: PropTypes.bool.isRequired,
-    setShow: PropTypes.func.isRequired,
-    title: PropTypes.node.isRequired,
-    children: PropTypes.node.isRequired,
-  };
-  
+  show: PropTypes.bool.isRequired,
+  setShow: PropTypes.func.isRequired,
+  title: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
+};
--- src/components/User/BannerAvatar/BannerAvatar.js
+++ src/components/User/BannerAvatar/BannerAvatar.js
@@ -7,48 +7,54 @@ import ConfigModal from "../../Modal/ConfigModal";
 import "./BannerAvatar.scss";
 
 export default function BannerAvatar(props) {
-    const { user, authUser } = props;
-    // Correct usage of useState
-    const [showModal, setShowModal] = useState(false);
-    const iconUrl = user?.icon ? user.icon : IconNotFound;
-    const bannerUrl = user?.banner ? user.banner : null;
+  const { user, authUser } = props;
+  // Correct usage of useState
+  const [showModal, setShowModal] = useState(false);
+  const iconUrl = user?.icon ? user.icon : IconNotFound;
+  const bannerUrl = user?.banner ? user.banner : null;
 
-    return (
-        <div
-            className="banner-icon"
-            style={{ backgroundImage: `url(${bannerUrl})` }}
-        >
-            <div className="icon" style={{ backgroundImage: `url(${iconUrl})` }} />
-            {user && (
-                <div className="options">
-                    {authUser.sub !== user.id && <Button onClick={() => setShowModal(true)}>プロフィール編集</Button>}
-                    {authUser.sub !== user.id && <Button>フォローする</Button>}
-                </div>
-            )}
-
-            <ConfigModal show={showModal} setShow={setShowModal} title="プロフィール編集">
-                <p>dddd</p>
-                <p>dddd</p>
-                <p>dddd</p>
-                <p>dddd</p>
-                <p>dddd</p>
-                <p>dddd</p>
-                <p>dddd</p>
-                <p>dddd</p>
-                <p>dddd</p>
-                <p>dddd</p>
-            </ConfigModal>
+  return (
+    <div
+      className="banner-icon"
+      style={{ backgroundImage: `url(${bannerUrl})` }}
+    >
+      <div className="icon" style={{ backgroundImage: `url(${iconUrl})` }} />
+      {user && (
+        <div className="options">
+          {authUser.sub !== user.id && (
+            <Button onClick={() => setShowModal(true)}>プロフィール編集</Button>
+          )}
+          {authUser.sub !== user.id && <Button>フォローする</Button>}
         </div>
-    );
+      )}
+
+      <ConfigModal
+        show={showModal}
+        setShow={setShowModal}
+        title="プロフィール編集"
+      >
+        <p>dddd</p>
+        <p>dddd</p>
+        <p>dddd</p>
+        <p>dddd</p>
+        <p>dddd</p>
+        <p>dddd</p>
+        <p>dddd</p>
+        <p>dddd</p>
+        <p>dddd</p>
+        <p>dddd</p>
+      </ConfigModal>
+    </div>
+  );
 }
 
 BannerAvatar.propTypes = {
-    user: PropTypes.shape({
-        icon: PropTypes.string,
-        banner: PropTypes.string,
-        id: PropTypes.number,
-    }),
-    authUser: PropTypes.shape({
-        sub: PropTypes.string,
-    }),
+  user: PropTypes.shape({
+    icon: PropTypes.string,
+    banner: PropTypes.string,
+    id: PropTypes.number,
+  }),
+  authUser: PropTypes.shape({
+    sub: PropTypes.string,
+  }),
 };
