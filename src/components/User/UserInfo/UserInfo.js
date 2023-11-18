317f36958db433d10cc8e5c32eded521c7b7122a
志村啓太 <keitashimura2023@gmail.com>
2023-11-18 21:40:26 +0900

    feat: その他のユーザー情報を表示

--- src/components/User/UserInfo/UserInfo.js
+++ src/components/User/UserInfo/UserInfo.js
@@ -1,22 +1,52 @@
 import React from "react";
+import moment from "moment";
+import localization from "moment/locale/ja";
+import { Location, Link, DataBirth } from "../../../utils/icons";
 import PropTypes from "prop-types";
-// import { Location, Link, DataBirth } from "../../../utils/icon";
 import "./UserInfo.scss";
 
 export default function UserInfo(props) {
   const { user } = props;
   return (
     <div className="user-info">
-      <div className="name">{user?.name}</div>
+      <h2 className="name">{user?.name}</h2>
       {user?.bio && <div className="bio">{user?.bio}</div>}
+      <div className="more-info">
+        {user?.location && (
+          <p>
+            <Location />
+            {user.location}
+          </p>
+        )}
+        {user?.website && (
+          <p>
+            <a
+              href={user.website}
+              alt={user.website}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Link /> {user.website}
+            </a>
+          </p>
+        )}
+        {user?.birth_date && (
+          <p>
+            <DataBirth />
+            {moment(user.birth_date).locale("ja", localization).format("LL")}
+          </p>
+        )}
+      </div>
     </div>
   );
 }
 
-// Define PropTypes for UserInfo component
 UserInfo.propTypes = {
   user: PropTypes.shape({
     name: PropTypes.string,
     bio: PropTypes.string,
+    location: PropTypes.string,
+    website: PropTypes.string,
+    birth_date: PropTypes.string,
   }),
 };
--- src/utils/icon.js
+++
@@ -1,3 +0,0 @@
-export { ReactComponent as Location } from "../assets/scg/location.svg";
-export { ReactComponent as Link } from "../assets/scg/link.svg";
-export { ReactComponent as DataBirth } from "../assets/scg/data-birth.svg";
