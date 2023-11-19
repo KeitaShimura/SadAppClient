31bd9d899a8c9b93484b9e99f15666a30546a8ce
志村啓太 <keitashimura2023@gmail.com>
2023-11-20 03:24:50 +0900

    feat: 投稿モーダルのUI作成

--- src/components/PostModal/PostModal.js
+++ src/components/PostModal/PostModal.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { Modal } from "react-bootstrap";
+import { Button, Modal, Form } from "react-bootstrap";
 import { Close } from "../../utils/icons";
 import "./PostModal.scss";
 
 export default function PostModal(props) {
   const { show, setShow } = props;
+  const [message, setMessage] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log("Message submitted:", message);
+  };
+
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+  };
 
   return (
     <Modal
@@ -20,13 +30,24 @@ export default function PostModal(props) {
           <Close onClick={() => setShow(false)} />
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body>...</Modal.Body>
+      <Modal.Body>
+        <Form onSubmit={onSubmit}>
+          <Form.Control
+            as="textarea"
+            rows={6}
+            value={message}
+            onChange={handleChange}
+            placeholder="今の気持ちを共有してみましょう！"
+          />
+          <span className="count">{message.length}</span>
+          <Button type="submit">投稿する</Button>
+        </Form>
+      </Modal.Body>
     </Modal>
   );
 }
 
 PostModal.propTypes = {
-  show: PropTypes.bool.isRequired, // 'show' プロパティの型情報を指定し、isRequiredを使って必須フィールドであることを示す
-  setShow: PropTypes.func.isRequired, // 'setShow' プロパティの型情報を指定し、isRequiredを使って必須フィールドであることを示す
-  children: PropTypes.node.isRequired, // 'children' プロパティの型情報を指定し、isRequiredを使って必須フィールドであることを示す
+  show: PropTypes.bool.isRequired,
+  setShow: PropTypes.func.isRequired,
 };
