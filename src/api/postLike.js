f12abfd514e4034f83bab983e1d334531d7387ed
志村啓太 <keitashimura2023@gmail.com>
2023-11-25 21:31:52 +0900

    feat: いいね数の表示機能の実装

--- src/api/postLike.js
+++ src/api/postLike.js
@@ -3,53 +3,64 @@ import { API_HOST } from "../utils/constant";
 import { getTokenApi } from "./auth";
 
 export function likePostApi(postId) {
-    const url = `${API_HOST}/api/user/post/${postId}/like`;
+  const url = `${API_HOST}/api/user/post/${postId}/like`;
 
-    return axios.post(
-        url,
-        {},
-        {
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${getTokenApi()}`,
-            },
-            withCredentials: true,
+  return axios
+    .post(
+      url,
+      {},
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${getTokenApi()}`,
         },
+        withCredentials: true,
+      },
     )
-        .then(response => response.data)
-        .catch(error => console.error("Like Error:", error));
+    .then((response) => response.data)
+    .catch((error) => console.error("Like Error:", error));
 }
 
 export function unlikePostApi(postId) {
-    const url = `${API_HOST}/api/user/post/${postId}/unlike`;
+  const url = `${API_HOST}/api/user/post/${postId}/unlike`;
 
-    return axios.delete(
-        url,
-        {
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${getTokenApi()}`,
-            },
-            withCredentials: true,
-        },
-    )
-        .then(response => response.data)
-        .catch(error => console.error("Unlike Error:", error));
+  return axios
+    .delete(url, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getTokenApi()}`,
+      },
+      withCredentials: true,
+    })
+    .then((response) => response.data)
+    .catch((error) => console.error("Unlike Error:", error));
 }
 
 export function checkIfPostLikedApi(postId) {
-    const url = `${API_HOST}/api/user/post/${postId}/checklike`;
+  const url = `${API_HOST}/api/user/post/${postId}/checklike`;
 
-    return axios.get(
-        url,
-        {
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${getTokenApi()}`,
-            },
-            withCredentials: true,
-        },
-    )
-        .then(response => response.data)
-        .catch(error => console.error("Check Like Error:", error));
+  return axios
+    .get(url, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getTokenApi()}`,
+      },
+      withCredentials: true,
+    })
+    .then((response) => response.data)
+    .catch((error) => console.error("Check Like Error:", error));
+}
+
+export function getLikesForPostApi(postId) {
+  const url = `${API_HOST}/api/user/post/${postId}/likes`;
+
+  return axios
+    .get(url, {
+      headers: {
+        Authorization: `Bearer ${getTokenApi()}`,
+      },
+      withCredentials: true,
+    })
+    .then((response) => response.data)
+    .catch((error) => console.error("Error fetching post likes:", error));
 }
--- src/components/ListPosts/ListPosts.js
+++ src/components/ListPosts/ListPosts.js
@@ -6,7 +6,12 @@ import { map } from "lodash";
 import { replaceURLWithHTMLLinks } from "../../utils/functions";
 import "./ListPosts.scss";
 import IconNotFound from "../../assets/png/icon-no-found.png";
-import { checkIfPostLikedApi, likePostApi, unlikePostApi } from "../../api/postLike";
+import {
+  checkIfPostLikedApi,
+  likePostApi,
+  unlikePostApi,
+  getLikesForPostApi,
+} from "../../api/postLike";
 import useAuth from "../../hooks/useAuth";
 
 export default function ListPosts(props) {
@@ -31,24 +36,47 @@ ListPosts.propTypes = {
 function Post(props) {
   const { post } = props;
   const [isLiked, setIsLiked] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
   const authUser = useAuth();
-  console.log(authUser.sub);
 
   useEffect(() => {
-    checkIfPostLikedApi(post.id, authUser.id)
-      .then((likeStatus) => setIsLiked(likeStatus))
-      .catch((error) => console.error("Check Like Error:", error));
-  }, [post.id]);
+    const fetchLikeData = async () => {
+      try {
+        const likeStatus = await checkIfPostLikedApi(post.id, authUser.id);
+        setIsLiked(likeStatus);
+        updateLikeCount();
+      } catch (error) {
+        console.error("Error fetching like data:", error);
+      }
+    };
+
+    fetchLikeData();
+  }, [post.id, authUser.id]);
+
+  const updateLikeCount = async () => {
+    try {
+      const likesData = await getLikesForPostApi(post.id);
+      setLikeCount(likesData.length);
+    } catch (error) {
+      console.error("Error fetching like count:", error);
+    }
+  };
 
   const handleLike = () => {
     likePostApi(post.id)
-      .then(() => setIsLiked(true))
+      .then(() => {
+        setIsLiked(true);
+        updateLikeCount();
+      })
       .catch((error) => console.error("Like Error:", error));
   };
 
   const handleUnlike = () => {
     unlikePostApi(post.id)
-      .then(() => setIsLiked(false))
+      .then(() => {
+        setIsLiked(false);
+        updateLikeCount();
+      })
       .catch((error) => console.error("Unlike Error:", error));
   };
 
@@ -58,26 +86,25 @@ function Post(props) {
     <div className="post">
       <Image className="icon" src={iconUrl} roundedCircle />
       <div>
-        <div className="name">
-          {post.user && (
-            <div className="name">
-              {post.user.name}
-              <span>{moment(post.created_at).calendar()}</span>
-            </div>
-          )}
-        </div>
+        {post.user && (
+          <div className="name">
+            {post.user.name}
+            <span>{moment(post.created_at).calendar()}</span>
+          </div>
+        )}
         <div
           dangerouslySetInnerHTML={{
             __html: replaceURLWithHTMLLinks(post.content),
           }}
-        ></div>
-      </div>
-      <div>
-        {isLiked ? (
-          <button onClick={handleUnlike}>いいね済み</button>
-        ) : (
-          <button onClick={handleLike}>いいねする</button>
-        )}
+        />
+        <div>
+          {isLiked ? (
+            <button onClick={handleUnlike}>いいね済み</button>
+          ) : (
+            <button onClick={handleLike}>いいねする</button>
+          )}
+          <span>{likeCount} Likes</span>
+        </div>
       </div>
     </div>
   );
@@ -85,12 +112,12 @@ function Post(props) {
 
 Post.propTypes = {
   post: PropTypes.shape({
+    id: PropTypes.number.isRequired,
     user: PropTypes.shape({
       icon: PropTypes.string,
       name: PropTypes.string,
     }),
-    id: PropTypes.number.isRequired,
-    content: PropTypes.string.isRequired,
     created_at: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
   }).isRequired,
 };
