08e59d07bb38d5abca1f62a0707803299bebb297
志村啓太 <keitashimura2023@gmail.com>
2023-11-25 21:34:29 +0900

    feat: eventLikeのAPI実装

---
+++ src/api/eventLike.js
@@ -0,0 +1,66 @@
+import axios from "axios";
+import { API_HOST } from "../utils/constant";
+import { getTokenApi } from "./auth";
+
+export function likeEventApi(eventId) {
+  const url = `${API_HOST}/api/user/event/${eventId}/like`;
+
+  return axios
+    .post(
+      url,
+      {},
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${getTokenApi()}`,
+        },
+        withCredentials: true,
+      },
+    )
+    .then((response) => response.data)
+    .catch((error) => console.error("Like Error:", error));
+}
+
+export function unlikeEventApi(eventId) {
+  const url = `${API_HOST}/api/user/event/${eventId}/unlike`;
+
+  return axios
+    .delete(url, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getTokenApi()}`,
+      },
+      withCredentials: true,
+    })
+    .then((response) => response.data)
+    .catch((error) => console.error("Unlike Error:", error));
+}
+
+export function checkIfEventLikedApi(eventId) {
+  const url = `${API_HOST}/api/user/event/${eventId}/checklike`;
+
+  return axios
+    .get(url, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getTokenApi()}`,
+      },
+      withCredentials: true,
+    })
+    .then((response) => response.data)
+    .catch((error) => console.error("Check Like Error:", error));
+}
+
+export function getLikesForEventApi(eventId) {
+  const url = `${API_HOST}/api/user/event/${eventId}/likes`;
+
+  return axios
+    .get(url, {
+      headers: {
+        Authorization: `Bearer ${getTokenApi()}`,
+      },
+      withCredentials: true,
+    })
+    .then((response) => response.data)
+    .catch((error) => console.error("Error fetching event likes:", error));
+}
