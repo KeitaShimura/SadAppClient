56102023b58539dbda34233390c86a13b5d613e5
志村啓太 <keitashimura2023@gmail.com>
2023-11-25 21:52:13 +0900

    feat: イベントのいいね機能実装

--- src/components/ListEvents/ListEvents.js
+++ src/components/ListEvents/ListEvents.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import moment from "moment";
 import { Image } from "react-bootstrap";
@@ -6,6 +6,13 @@ import { map } from "lodash";
 import { replaceURLWithHTMLLinks } from "../../utils/functions";
 import "./ListEvents.scss";
 import IconNotFound from "../../assets/png/icon-no-found.png";
+import useAuth from "../../hooks/useAuth";
+import {
+  checkIfEventLikedApi,
+  getLikesForEventApi,
+  likeEventApi,
+  unlikeEventApi,
+} from "../../api/eventLike";
 
 export default function ListEvents(props) {
   const { events } = props;
@@ -28,6 +35,51 @@ ListEvents.propTypes = {
 
 function Event(props) {
   const { event } = props;
+  const [isLiked, setIsLiked] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
+  const authUser = useAuth(); // Assuming useAuth returns the authenticated user
+
+  useEffect(() => {
+    const fetchLikeData = async () => {
+      try {
+        const likeStatus = await checkIfEventLikedApi(event.id, authUser.id);
+        console.log("Like status for event:", likeStatus); // Debugging output
+        setIsLiked(likeStatus);
+        updateLikeCount();
+      } catch (error) {
+        console.error("Error fetching like data:", error);
+      }
+    };
+
+    fetchLikeData();
+  }, [event.id, authUser.id]);
+
+  const updateLikeCount = async () => {
+    try {
+      const likesData = await getLikesForEventApi(event.id);
+      setLikeCount(likesData.length);
+    } catch (error) {
+      console.error("Error fetching like count:", error);
+    }
+  };
+
+  const handleLike = () => {
+    likeEventApi(event.id)
+      .then(() => {
+        setIsLiked(true);
+        updateLikeCount();
+      })
+      .catch((error) => console.error("Like Error:", error));
+  };
+
+  const handleUnlike = () => {
+    unlikeEventApi(event.id)
+      .then(() => {
+        setIsLiked(false);
+        updateLikeCount();
+      })
+      .catch((error) => console.error("Unlike Error:", error));
+  };
 
   const iconUrl = event.user?.icon ? event.user.icon : IconNotFound;
 
@@ -65,12 +117,21 @@ function Event(props) {
           </div>
         </div>
       </div>
+      <div>
+        {isLiked ? (
+          <button onClick={handleUnlike}>いいね済み</button>
+        ) : (
+          <button onClick={handleLike}>いいねする</button>
+        )}
+        <span>{likeCount} Likes</span>
+      </div>
     </div>
   );
 }
 
 Event.propTypes = {
   event: PropTypes.shape({
+    id: PropTypes.number.isRequired,
     user: PropTypes.shape({
       icon: PropTypes.string,
       name: PropTypes.string,
