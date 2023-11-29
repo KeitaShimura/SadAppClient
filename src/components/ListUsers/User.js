4ae923943a37de11dff7d77e5b06c74d20080a1b
志村啓太 <keitashimura2023@gmail.com>
2023-11-29 17:09:51 +0900

    feat: ユーザー一覧からのフォロー機能追加

--- src/components/ListUsers/User.js
+++ src/components/ListUsers/User.js
@@ -2,16 +2,44 @@ import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { getUserApi } from "../../api/user";
 import { toast } from "react-toastify";
-import { Image } from "react-bootstrap";
+import { Button, Image } from "react-bootstrap";
 import IconNotFound from "../../assets/png/icon-no-found.png";
 import { useNavigate } from "react-router";
+import { checkIfFollowingApi, followUserApi, unfollowUserApi } from "../../api/follow";
+import useAuth from "../../hooks/useAuth";
 
 export default function User(props) {
   const { user } = props;
   const [userInfo, setUserInfo] = useState(null);
   const iconUrl = user?.icon ? user.icon : IconNotFound;
+  const [isFollowing, setIsFollowing] = useState(false);
 
   const navigate = useNavigate();
+  const authUser = useAuth();
+
+  const handleFollow = (userId, event) => {
+    event.preventDefault(); // This should now work without error
+    followUserApi(userId)
+      .then(() => {
+        console.log("User ID:", userId);
+        setIsFollowing(true);
+        // Additional logic if needed
+      })
+      .catch((error) => {
+        console.error("Follow Error:", error);
+      });
+  };
+
+  const handleUnfollow = () => {
+    unfollowUserApi(user.id)
+      .then(() => {
+        setIsFollowing(false);
+        // Additional logic if needed
+      })
+      .catch((error) => {
+        console.error("Unfollow Error:", error);
+      });
+  };
 
   const handleShowUser = (userId) => {
     navigate(`/user/${userId}`);
@@ -28,6 +56,24 @@ export default function User(props) {
       });
   }, [user]);
 
+  useEffect(() => {
+    // フォロー状態をチェックする関数
+    const checkFollowStatus = async () => {
+      try {
+        console.log(authUser.sub, user.id);
+        const status = await checkIfFollowingApi(authUser.sub, user.id); // 実際のAPIコールに置き換えてください
+        setIsFollowing(status);
+      } catch (error) {
+        console.error("フォロー状態チェックエラー:", error);
+      }
+    };
+
+    if (user && authUser) {
+      checkFollowStatus();
+    }
+  }, [user, authUser]); // user と authUser に依存
+
+
   return (
     <div className="user" onClick={() => handleShowUser(user.id)}>
       <Image className="icon" src={iconUrl} roundedCircle />
@@ -39,6 +85,30 @@ export default function User(props) {
           </div>
         </div>
       )}
+      {Number(authUser.sub) !== user.id &&
+        (isFollowing ? (
+          <Button
+            className="unfollow"
+            onClick={(e) => {
+              e.stopPropagation();
+              handleUnfollow(user.id, e);
+            }}
+          >
+            <span>フォロー解除</span>
+          </Button>
+        ) : (
+
+          <Button
+            className="unfollow"
+            onClick={(e) => {
+              e.stopPropagation();
+              handleFollow(user.id, e);
+            }}
+          >
+            フォローする
+          </Button>
+
+        ))}
     </div>
   );
 }
