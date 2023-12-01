e691941d80f0098401d84d44a8ec638ce3f86d3f
志村啓太 <keitashimura2023@gmail.com>
2023-12-02 07:35:48 +0900

    feat: サイドメニュー追加

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.scss'; // Import SCSS module
 import { Button, Image } from "react-bootstrap";
 import Logo from "../../assets/png/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome, faPowerOff, faUser, faUsers, faVideo } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faHome, faPowerOff, faUser, faUsers, faVideo } from "@fortawesome/free-solid-svg-icons";
 import useAuth from "../../hooks/useAuth";
 import { logoutApi } from "../../api/auth";
 import PostModal from "../Modal/PostModal";
@@ -19,8 +19,42 @@ const Header = () => {
         logoutApi();
         window.location.reload();
     };
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
+
     return (
-        <header className="header">
+
+        <header className={`header ${showMobileMenu ? 'show-mobile-menu' : ''}`}>
+
+            <FontAwesomeIcon icon={faBars} className="hamburger-menu" onClick={() => setShowMobileMenu(!showMobileMenu)} />
+            <div className="mobile-menu">
+                <a href="/" onClick={() => setShowMobileMenu(false)}>
+                    <FontAwesomeIcon icon={faHome} />
+                    投稿一覧
+                </a>
+                <a href="/events" onClick={() => setShowMobileMenu(false)}>
+                    <FontAwesomeIcon icon={faVideo} />
+                    イベント一覧
+                </a>
+                <a href={`/users/${user?.sub}`} onClick={() => setShowMobileMenu(false)}>
+                    <FontAwesomeIcon icon={faUsers} />
+                    ユーザー一覧
+                </a>
+                <a href={`/user/${user?.sub}`} onClick={() => setShowMobileMenu(false)}>
+                    <FontAwesomeIcon icon={faUser} />
+                    プロフィール
+                </a>
+                <a href="" onClick={() => { logout(); setShowMobileMenu(false); }}>
+                    <FontAwesomeIcon icon={faPowerOff} />
+                    ログアウト
+                </a>
+                <Button onClick={() => { setShowPostModal(true); setShowMobileMenu(false); }}>
+                    投稿する
+                </Button>
+                <Button onClick={() => { setShowEventModal(true); setShowMobileMenu(false); }}>
+                    イベントを投稿する
+                </Button>
+            </div>
+
             <Image className="logo" src={Logo} width={40} />
             <a href="/">
                 <FontAwesomeIcon icon={faHome} />
