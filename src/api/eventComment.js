0c972cf9aa2fa7ea9bfee80bd001e4a03fa6bf22
志村啓太 <keitashimura2023@gmail.com>
2023-12-01 19:18:58 +0900

    feat: eventComment.jsのコメント追記

--- src/api/eventComment.js
+++ src/api/eventComment.js
@@ -2,52 +2,113 @@ import axios from "axios";
 import { API_HOST } from "../utils/constant";
 import { getTokenApi } from "./auth";
 
-// Fetch all comments for a event
+
+// イベントのすべてのコメントを取得
 export function getEventCommentsApi(eventId) {
-  return axios.get(`${API_HOST}/api/user/events/comments/${eventId}`, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${getTokenApi()}`,
-    },
-  });
+  return axios
+    .get(`${API_HOST}/api/user/events/comments/${eventId}`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getTokenApi()}`,
+      },
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      // エラーハンドリング
+      if (error.response) {
+        // サーバーからのエラーレスポンス
+        throw new Error(error.response.data.message);
+      } else if (error.request) {
+        // リクエストがサーバーに到達しなかった場合
+        throw new Error("ネットワークエラー：リクエストが送信されませんでした。");
+      } else {
+        // その他のエラー
+        throw new Error("エラーが発生しました：" + error.message);
+      }
+    });
 }
 
-// Create a new comment
+// 新しいコメントを作成
 export function createEventCommentApi(eventId, commentData) {
-  return axios.post(
-    `${API_HOST}/api/user/events/comments/${eventId}`,
-    commentData,
-    {
+  return axios
+    .post(`${API_HOST}/api/user/events/comments/${eventId}`, commentData, {
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${getTokenApi()}`,
       },
       withCredentials: true,
-    },
-  );
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      // エラーハンドリング
+      if (error.response) {
+        // サーバーからのエラーレスポンス
+        throw new Error(error.response.data.message);
+      } else if (error.request) {
+        // リクエストがサーバーに到達しなかった場合
+        throw new Error("ネットワークエラー：リクエストが送信されませんでした。");
+      } else {
+        // その他のエラー
+        throw new Error("エラーが発生しました：" + error.message);
+      }
+    });
 }
 
-// Update a comment
+// コメントを更新
 export function updateEventCommentApi(commentId, commentData) {
-  return axios.put(
-    `${API_HOST}/api/user/events/comments/${commentId}`,
-    commentData,
-    {
+  return axios
+    .put(`${API_HOST}/api/user/events/comments/${commentId}`, commentData, {
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${getTokenApi()}`,
       },
       withCredentials: true,
-    },
-  );
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      // エラーハンドリング
+      if (error.response) {
+        // サーバーからのエラーレスポンス
+        throw new Error(error.response.data.message);
+      } else if (error.request) {
+        // リクエストがサーバーに到達しなかった場合
+        throw new Error("ネットワークエラー：リクエストが送信されませんでした。");
+      } else {
+        // その他のエラー
+        throw new Error("エラーが発生しました：" + error.message);
+      }
+    });
 }
 
-// Delete a comment
+// コメントを削除
 export function deleteEventCommentApi(commentId) {
-  return axios.delete(`${API_HOST}/api/user/events/comments/${commentId}`, {
-    headers: {
-      Authorization: `Bearer ${getTokenApi()}`,
-    },
-    withCredentials: true,
-  });
+  return axios
+    .delete(`${API_HOST}/api/user/events/comments/${commentId}`, {
+      headers: {
+        Authorization: `Bearer ${getTokenApi()}`,
+      },
+      withCredentials: true,
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      // エラーハンドリング
+      if (error.response) {
+        // サーバーからのエラーレスポンス
+        throw new Error(error.response.data.message);
+      } else if (error.request) {
+        // リクエストがサーバーに到達しなかった場合
+        throw new Error("ネットワークエラー：リクエストが送信されませんでした。");
+      } else {
+        // その他のエラー
+        throw new Error("エラーが発生しました：" + error.message);
+      }
+    });
 }
