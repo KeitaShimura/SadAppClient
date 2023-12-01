4602ebdb70b8cf568d2ca2fba0ac76d0bbc99705
志村啓太 <keitashimura2023@gmail.com>
2023-12-01 19:36:11 +0900

    feat: postLike.jsにエラーハンドリング追加

--- src/api/postLike.js
+++ src/api/postLike.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { API_HOST } from "../utils/constant";
 import { getTokenApi } from "./auth";
 
+// 投稿を「いいね」する
 export function likePostApi(postId) {
   const url = `${API_HOST}/api/user/posts/${postId}/like`;
 
@@ -18,9 +19,13 @@ export function likePostApi(postId) {
       },
     )
     .then((response) => response.data)
-    .catch((error) => console.error("Like Error:", error));
+    .catch((error) => {
+      console.error("「いいね」エラー:", error);
+      throw error;
+    });
 }
 
+// 投稿の「いいね」を取り消す
 export function unlikePostApi(postId) {
   const url = `${API_HOST}/api/user/posts/${postId}/unlike`;
 
@@ -33,9 +38,13 @@ export function unlikePostApi(postId) {
       withCredentials: true,
     })
     .then((response) => response.data)
-    .catch((error) => console.error("Unlike Error:", error));
+    .catch((error) => {
+      console.error("「いいね」取り消しエラー:", error);
+      throw error;
+    });
 }
 
+// 投稿に「いいね」したかどうかを確認
 export function checkIfPostLikedApi(postId) {
   const url = `${API_HOST}/api/user/posts/${postId}/check_like`;
 
@@ -48,9 +57,13 @@ export function checkIfPostLikedApi(postId) {
       withCredentials: true,
     })
     .then((response) => response.data)
-    .catch((error) => console.error("Check Like Error:", error));
+    .catch((error) => {
+      console.error("「いいね」確認エラー:", error);
+      throw error;
+    });
 }
 
+// 投稿の「いいね」を取得
 export function getLikesForPostApi(postId) {
   const url = `${API_HOST}/api/user/posts/${postId}/likes`;
 
@@ -60,5 +73,10 @@ export function getLikesForPostApi(postId) {
       Authorization: `Bearer ${getTokenApi()}`,
     },
     withCredentials: true,
-  });
+  })
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error("投稿の「いいね」を取得中にエラーが発生しました:", error);
+      throw error;
+    });
 }
