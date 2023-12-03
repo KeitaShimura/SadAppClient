0406bfcf8c88396da6b752e29a9bddfad3cc5743
志村啓太 <keitashimura2023@gmail.com>
2023-12-03 22:19:23 +0900

    feat: 戻るボタンを設置

--- src/layout/BasicLayout/BasicLayout.js
+++ src/layout/BasicLayout/BasicLayout.js
@@ -1,18 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 import "./BasicLayout.scss";
 import Header from "../../components/Header/Header";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 export default function BasicLayout(props) {
   const { className, children } = props;
+  const navigate = useNavigate();
+
+  // 戻るボタンのクリックイベントハンドラ
+  const handleBackButtonClick = () => {
+    navigate(-1); // 1ステップ前に戻る
+  };
 
   return (
     <Container className={`basic-layout ${className}`}>
       <Row>
         <Header />
         <Col xs={12} className="basic-layout__content">
+          <Button className="back-button" onClick={handleBackButtonClick}>
+            <FontAwesomeIcon icon={faArrowLeft} /> 戻る
+          </Button>
           {children}
         </Col>
       </Row>
