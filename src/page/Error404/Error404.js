2b52efdcf93e1d7f6bf3b1a8650da1da592ef9a4
志村啓太 <keitashimura2023@gmail.com>
2023-12-08 01:43:03 +0900

    refactor: ESLint/Prettierの実行

--- src/page/Error404/Error404.js
+++ src/page/Error404/Error404.js
@@ -1,14 +1,19 @@
 import React from "react";
-import Logo from "../../assets/png/error-404.png";
+import Logo from "../../assets/png/logo.png";
 import "./Error404.scss";
 import { Link } from "react-router-dom";
+import { Image } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 export default function Error404() {
   return (
     <div className="error404">
-      <h2>COCOLOTalk</h2>
-      <img src={Logo} alt="Error404" />
-      <Link to="/">ホームへ戻る</Link>
+      <h2>ページが存在しません。</h2>
+      <Image className="logo" src={Logo} width={200} />
+      <Link to="/">
+        <FontAwesomeIcon icon={faArrowLeft} /> ホームへ戻る
+      </Link>
     </div>
   );
 }
