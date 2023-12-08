de09576130b74b1261b0d0417f6d4f2069313caf
志村啓太 <keitashimura2023@gmail.com>
2023-12-09 03:53:41 +0900

    refactor: ESLint/Prettierの実行

--- src/api/auth.test.js
+++ src/api/auth.test.js
@@ -5,39 +5,39 @@ import { API_HOST } from "../utils/constant";
 jest.mock("axios"); // axiosモジュールをモック化
 
 test("registerApiがユーザーデータと共にPOSTリクエストを送信する", async () => {
-    const user = {
-        /* テスト用のユーザーデータ */
-    };
-    const responseData = { token: "testToken" };
-    axios.post.mockResolvedValue({ data: responseData }); // axios.postのモックを設定
+  const user = {
+    /* テスト用のユーザーデータ */
+  };
+  const responseData = { token: "testToken" };
+  axios.post.mockResolvedValue({ data: responseData }); // axios.postのモックを設定
 
-    const result = await registerApi(user);
+  const result = await registerApi(user);
 
-    // axios.postが適切に呼び出されたことを確認
-    expect(axios.post).toHaveBeenCalledWith(
-        `${API_HOST}/api/user/register`,
-        user,
-        { withCredentials: true },
-    );
+  // axios.postが適切に呼び出されたことを確認
+  expect(axios.post).toHaveBeenCalledWith(
+    `${API_HOST}/api/user/register`,
+    user,
+    { withCredentials: true },
+  );
 
-    // 適切なレスポンスが返されたことを確認
-    expect(result).toEqual(responseData);
+  // 適切なレスポンスが返されたことを確認
+  expect(result).toEqual(responseData);
 });
 
 test("loginApiがユーザーデータと共にPOSTリクエストを送信する", async () => {
-    const user = {
-        /* テスト用のユーザーデータ */
-    };
-    const responseData = { token: "testToken" };
-    axios.post.mockResolvedValue({ data: responseData }); // axios.postのモックを設定
+  const user = {
+    /* テスト用のユーザーデータ */
+  };
+  const responseData = { token: "testToken" };
+  axios.post.mockResolvedValue({ data: responseData }); // axios.postのモックを設定
 
-    const result = await loginApi(user);
+  const result = await loginApi(user);
 
-    // axios.postが適切に呼び出されたことを確認
-    expect(axios.post).toHaveBeenCalledWith(`${API_HOST}/api/user/login`, user, {
-        withCredentials: true,
-    });
+  // axios.postが適切に呼び出されたことを確認
+  expect(axios.post).toHaveBeenCalledWith(`${API_HOST}/api/user/login`, user, {
+    withCredentials: true,
+  });
 
-    // 適切なレスポンスが返されたことを確認
-    expect(result).toEqual(responseData);
+  // 適切なレスポンスが返されたことを確認
+  expect(result).toEqual(responseData);
 });
