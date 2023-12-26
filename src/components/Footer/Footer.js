e129b83575d99e2886a48fa16d026b90b16d27bf
志村啓太 <keitashimura2023@gmail.com>
2023-12-26 22:20:25 +0900

    faet: プライバシーポリシー/利用規約を一旦非表示に

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,8 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <footer className="footer">
-      <a href="/terms-of-service">利用規約</a>
-      <a href="/privacy-policy">プライバシーポリシー</a>
+      {/* <a href="/terms-of-service">利用規約</a>
+      <a href="/privacy-policy">プライバシーポリシー</a> */}
       <p>© 2023 COCOLOTalk</p>
     </footer>
   );
